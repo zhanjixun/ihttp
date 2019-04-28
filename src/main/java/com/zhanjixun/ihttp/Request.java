083521ef6bbc8d13844f76471d253c7dfd28ae57
zhanjixun <zhanjixun@chz-china.com>
2019-04-28 10:33:31 +0800

    push

--- src/main/java/com/zhanjixun/ihttp/Request.java
+++ src/main/java/com/zhanjixun/ihttp/Request.java
@@ -17,17 +17,22 @@ import java.util.List;
 public class Request {
 
     private String id;
-
+    //请求地址
     private String url;
+    //请求编码
     private String method;
-
+    //跟随重定向
     private boolean followRedirects = true;
-
+    //请求体
     private String body;
+    //请求字符编码
     private String charset;
 
+    //请求头
     private List<NameValuePair> headers = Lists.newArrayList();
+    //请求参数
     private List<NameValuePair> params = Lists.newArrayList();
+    //文件
     private List<FileParts> fileParts = Lists.newArrayList();
 
     @Override
