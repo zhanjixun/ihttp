e63c63bea77e2fec0cbd54115490e32b7114a706
zhanjixun <zhanjixun@qq.com>
2019-09-04 11:31:32 +0800

    负载均衡的URL

--- src/main/java/com/zhanjixun/ihttp/annotations/BalanceURL.java
+++ src/main/java/com/zhanjixun/ihttp/annotations/BalanceURL.java
@@ -12,7 +12,7 @@ import java.lang.annotation.Target;
  */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-public @interface SwitchURL {
+public @interface BalanceURL {
 
     String[] value() default {};
 
