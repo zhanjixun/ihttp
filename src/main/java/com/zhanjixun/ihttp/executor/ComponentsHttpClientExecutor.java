687406913e7a688d573781b20fa11f9648acd1bc
zhanjixun <zhanjixun@chz-china.com>
2019-11-06 17:24:16 +0800

    实现ComponentsHttpClientExecutor DELETE、PUT方法

--- src/main/java/com/zhanjixun/ihttp/executor/ComponentsHttpClientExecutor.java
+++ src/main/java/com/zhanjixun/ihttp/executor/ComponentsHttpClientExecutor.java
@@ -18,9 +18,7 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.CookieStore;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.client.methods.*;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
@@ -127,12 +125,57 @@ public class ComponentsHttpClientExecutor extends BaseExecutor {
 
 	@Override
 	protected Response doDeleteMethod(Request request) throws IOException {
-		return null;
+		request.setUrl(StrUtils.addQuery(request.getUrl(), request.getParams()));
+		request.getParams().clear();
+
+		HttpDelete method = new HttpDelete(request.getUrl());
+		method.setConfig(RequestConfig.custom().setRedirectsEnabled(request.isFollowRedirects()).build());
+		request.getHeaders().forEach(h -> method.addHeader(h.getName(), h.getValue()));
+		return executeMethod(method, request);
 	}
 
 	@Override
 	protected Response doPutMethod(Request request) throws IOException {
-		return null;
+		HttpPut method = new HttpPut(request.getUrl());
+		request.getHeaders().forEach(h -> method.addHeader(h.getName(), h.getValue()));
+
+		//带参数
+		String charset = Optional.ofNullable(request.getCharset()).orElse("UTF-8");
+		String paramString = request.getParams().stream().map(p -> p.getName() + "=" + p.getValue()).collect(Collectors.joining("&"));
+		if (StringUtils.isNotBlank(paramString)) {
+			method.setEntity(new StringEntity(paramString, ContentType.create("application/x-www-form-urlencoded", charset)));
+		}
+		//直接请求体
+		if (StringUtils.isNotBlank(request.getBody())) {
+			try {
+				Optional<String> contentType = Optional.ofNullable(method.getFirstHeader("Content-Type")).map(org.apache.http.NameValuePair::getValue);
+				if (contentType.isPresent()) {
+					StringEntity stringEntity = new StringEntity(request.getBody());
+					stringEntity.setContentType(contentType.get());
+					method.setEntity(stringEntity);
+				} else {
+					method.setEntity(new StringEntity(request.getBody(), ContentType.APPLICATION_JSON));
+				}
+			} catch (UnsupportedEncodingException e) {
+				throw new RuntimeException("构造请求体StringBody出错", e);
+			}
+		}
+		//带文件
+		if (CollectionUtils.isNotEmpty(request.getFileParts())) {
+			MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+			request.getParams().forEach(p -> builder.addTextBody(p.getName(), p.getValue()));
+
+			for (FileParts fileParts : request.getFileParts()) {
+				File file = fileParts.getFilePart();
+
+				String mimeType = new MimetypesFileTypeMap().getContentType(file.getName());
+				ContentType contentType = mimeType != null ? ContentType.create(mimeType) : ContentType.DEFAULT_BINARY;
+
+				builder.addBinaryBody(fileParts.getName(), file, contentType, file.getName());
+			}
+			method.setEntity(builder.build());
+		}
+		return executeMethod(method, request);
 	}
 
 	private Response executeMethod(HttpRequestBase method, Request request) throws IOException {
