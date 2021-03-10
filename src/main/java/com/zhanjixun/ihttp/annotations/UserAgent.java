4779d60136e7c31430e95a1d87c06a0ad24b19df
zhanjixun <jixun-zhan@msyc.cc>
2021-03-10 14:39:19 +0800

    默认的UserAgent

--- src/main/java/com/zhanjixun/ihttp/annotations/UserAgent.java
+++ src/main/java/com/zhanjixun/ihttp/annotations/UserAgent.java
@@ -6,5 +6,7 @@ import java.lang.annotation.*;
 @Target({ElementType.TYPE, ElementType.METHOD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface UserAgent {
-	String value() default "";
+
+    String value() default "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36";
+    
 }
