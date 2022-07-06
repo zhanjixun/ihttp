1c6ce2ab7a3d0d79480c1716a5ee3615825d1db2
zhanjixun <jim.zhan@miniso.com>
2022-07-06 11:07:23 +0800

    单元测试

--- src/test/java/com/zhanjixun/ihttp/test/SystemTest.java
+++ src/test/java/com/zhanjixun/ihttp/test/SystemTest.java
@@ -2,13 +2,13 @@ package com.zhanjixun.ihttp.test;
 
 import com.zhanjixun.ihttp.IHTTP;
 import com.zhanjixun.ihttp.Response;
-import com.zhanjixun.ihttp.annotations.GET;
-import com.zhanjixun.ihttp.annotations.Placeholder;
-import com.zhanjixun.ihttp.annotations.URL;
+import com.zhanjixun.ihttp.annotations.*;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import okhttp3.mockwebserver.RecordedRequest;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author :zhanjixun
@@ -19,41 +19,93 @@ public class SystemTest extends BaseTest {
     private final TestMapper mapper = IHTTP.getMapper(TestMapper.class);
 
     @URL("#{url}")
+    @RequestHeader(name = "headerName", value = "headerValue")
     public interface TestMapper {
+
+        default String testDefaultMethod() {
+            return "testDefaultMethod";
+        }
+
         @GET
         @URL("/testGet")
+        @RequestHeader(name = "headerName", value = "headerValue")
+        @RequestHeader(name = "headerName1", value = "headerValue1")
         Response testGet(@Placeholder("url") String url);
 
         @GET(followRedirects = false)
         @URL("/followRedirects")
         Response followRedirects(@Placeholder("url") String url);
-        
-        default String testDefaultMethod() {
-            return "TestMapper#testDefaultMethod";
-        }
+
+        @POST
+        @URL("/testPost")
+        Response testPost(@Placeholder("url") String url);
+
+        @PUT
+        @URL("/testPut")
+        Response testPut(@Placeholder("url") String url);
+
+        @DELETE
+        @URL("/testDelete")
+        Response testDelete(@Placeholder("url") String url);
     }
 
     @Test
-    public void test() throws Exception {
+    public void name1() {
+        assertEquals(mapper.testDefaultMethod(), "testDefaultMethod");
+    }
+
+    @Test
+    public void name2() throws Exception {
         MockWebServer mockWebServer = new MockWebServer();
-        mockWebServer.enqueue(new MockResponse().setResponseCode(200).setBody("ok"));
         String url = mockWebServer.url("/").toString();
-        Response response = mapper.testGet(url);
-        assert response.getText().equals("ok");
-        RecordedRequest recordedRequest = mockWebServer.takeRequest();
-        assert "GET".equals(recordedRequest.getMethod());
-        assert "/testGet".equals(recordedRequest.getPath());
 
+        mockWebServer.enqueue(new MockResponse().setResponseCode(200).setBody("testGet"));
+        Response response = mapper.testGet(url);
+        assertEquals(response.getText(), "testGet");
+
+        RecordedRequest recordedRequest1 = mockWebServer.takeRequest();
+        assertEquals(recordedRequest1.getMethod(), "GET");
+        assertEquals(recordedRequest1.getPath(), "/testGet");
+
+        mockWebServer.enqueue(new MockResponse().setResponseCode(200).setBody("testPost"));
+        assertEquals(mapper.testPost(url).getText(), "testPost");
+        RecordedRequest recordedRequest2 = mockWebServer.takeRequest();
+        assertEquals(recordedRequest2.getMethod(), "POST");
+
+        mockWebServer.enqueue(new MockResponse().setResponseCode(200).setBody("testPut"));
+        assertEquals(mapper.testPut(url).getText(), "testPut");
+        RecordedRequest recordedRequest3 = mockWebServer.takeRequest();
+        assertEquals(recordedRequest3.getMethod(), "PUT");
+
+        mockWebServer.enqueue(new MockResponse().setResponseCode(200).setBody("testDelete"));
+        assertEquals(mapper.testDelete(url).getText(), "testDelete");
+        RecordedRequest recordedRequest4 = mockWebServer.takeRequest();
+        assertEquals(recordedRequest4.getMethod(), "PUT");
+
+        //测试get请求重定向
         mockWebServer.enqueue(new MockResponse().setResponseCode(302).addHeader("Location", url + "/redirect"));
         mockWebServer.enqueue(new MockResponse().setResponseCode(200).setBody("ok"));
-        assert mapper.testGet(url).getStatus() == 200;
+        Response response1 = mapper.testGet(url);
+        assertEquals(response1.getStatus(), 200);
+        assertEquals(response1.getText(), "ok");
 
         mockWebServer.enqueue(new MockResponse().setResponseCode(302).addHeader("Location", url + "/redirect"));
-        assert mapper.followRedirects(url).getStatus() == 302;
+        Response response2 = mapper.followRedirects(url);
+        assertEquals(response2.getStatus(), 302);
+        assertEquals(response2.getLocation(), url + "/redirect");
     }
 
     @Test
-    public void name2() {
-        assert mapper.testDefaultMethod().equals("TestMapper#testDefaultMethod");
+    public void name3() throws InterruptedException {
+        MockWebServer mockWebServer = new MockWebServer();
+        String url = mockWebServer.url("/").toString();
+
+        mockWebServer.enqueue(new MockResponse().setResponseCode(200).setBody("testGet"));
+        Response response = mapper.testGet(url);
+        assertEquals(response.getText(), "testGet");
+        RecordedRequest recordedRequest = mockWebServer.takeRequest();
+
+        assertEquals(recordedRequest.getHeader("headerName"), "headerValue");
+        assertEquals(recordedRequest.getHeader("headerName1"), "headerValue1");
     }
 }
